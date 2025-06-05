cf0ab478b0e66ec400df7c34c85968ad9ac1d94c
wesley <wesley@bitechular.com>
2025-06-05 11:54:31 +0200

    Refactor new brew and header to one file

--- app/brew/new.tsx
+++ app/brew/new.tsx
@@ -1,11 +1,11 @@
-import NewBrewHeader from "@/app/brew/new-brew-header";
 import {Brew} from "@/models/brew"
 import {BrewService} from "@/services/brew-service"
 import ThemedTextInput from "@/ui/components/themed-textinput"
 import Ionicons from "@expo/vector-icons/Ionicons";
-import {useRouter} from "expo-router"
+import {useRouter, Link} from "expo-router"
 import React, {useState} from "react"
 import {Text, TouchableOpacity, View} from "react-native"
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 const NewBrew: React.FC = () => {
     const [name, setName] = useState("");
@@ -67,3 +67,39 @@ const NewBrew: React.FC = () => {
 };
 
 export default NewBrew;
+
+const NewBrewHeader: React.FC = () => {
+    const insets = useSafeAreaInsets();
+
+    return (
+        <View
+            className="bg-green-500 overflow-hidden w-full z-50"
+            style={{
+                paddingTop: insets.top,
+                borderBottomLeftRadius: 50,
+                borderBottomRightRadius: 50,
+            }}
+        >
+            {/* Decorative circles */}
+            <View className="absolute inset-0 pointer-events-none">
+                <View className="absolute -top-[140px] -right-[50px] w-56 h-56 rounded-full bg-white/15"/>
+                <View className="absolute top-20 right-16 w-20 h-20 rounded-full bg-white/15"/>
+                <View className="absolute bottom-0 -right-[100px] w-40 h-40 rounded-full bg-white/15"/>
+            </View>
+
+            <View className="px-4 pb-8">
+                <TouchableOpacity activeOpacity={0.8}>
+                    <Link href="/" className="text-brown-100 mt-4 mb-8">
+
+                        <View className="w-12 h-12 rounded-full border border-white justify-center items-center">
+                            <Ionicons name="chevron-back" size={20} color="white"/>
+                        </View>
+                    </Link>
+                </TouchableOpacity>
+
+
+                <Text className="text-5xl font-bold text-brown-100">Start new brew</Text>
+            </View>
+        </View>
+    );
+};
