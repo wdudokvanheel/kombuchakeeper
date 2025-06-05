a1edbb9ffa6e9054134c0d1f15039d3c0192e55d
wesley <wesley@bitechular.com>
2025-06-05 09:43:40 +0200

    Minor fixes for new Brew view and icon on save button

--- app/brew/new.tsx
+++ app/brew/new.tsx
@@ -2,6 +2,7 @@ import NewBrewHeader from "@/app/brew/new-brew-header";
 import {Brew} from "@/models/brew"
 import {BrewService} from "@/services/brew-service"
 import ThemedTextInput from "@/ui/components/themed-textinput"
+import Ionicons from "@expo/vector-icons/Ionicons";
 import {useRouter} from "expo-router"
 import React, {useState} from "react"
 import {Text, TouchableOpacity, View} from "react-native"
@@ -31,7 +32,7 @@ const NewBrew: React.FC = () => {
             <View className="flex-1 justify-between px-4 pt-4">
                 <View className="flex-1">
                     <Text className="text-lg font-semibold text-brown-900 mb-2">
-                        Brew Name
+                        Brew name
                     </Text>
 
                     <ThemedTextInput
@@ -41,7 +42,7 @@ const NewBrew: React.FC = () => {
                     />
 
                     <Text className="text-lg font-semibold text-gray-700 mb-2">
-                        First Fermentation duration
+                        First fermentation duration
                     </Text>
                     <ThemedTextInput
                         value={days}
@@ -53,9 +54,10 @@ const NewBrew: React.FC = () => {
                 <View>
                     <TouchableOpacity
                         onPress={saveBrew}
-                        className="bg-brown-800 rounded-[64px] py-4 items-center"
+                        className="bg-brown-800 rounded-[64px] py-4 px-6 flex-row items-center justify-center"
                     >
-                        <Text className="text-white text-xl font-semibold">Add Brew</Text>
+                        <Text className="text-white text-xl font-semibold">Add brew</Text>
+                        <Ionicons name="add" size={24} color="white" className="ml-2" />
                     </TouchableOpacity>
                 </View>
             </View>
