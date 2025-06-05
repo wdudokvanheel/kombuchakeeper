477a1ddc3338ee3a2e378e7ba3f3550ec642f874
wesley <wesley@bitechular.com>
2025-06-05 11:53:59 +0200

    Add header to brew details page

--- app/brew/[id].tsx
+++ app/brew/[id].tsx
@@ -1,23 +1,29 @@
 import {Brew} from '@/models/brew'
 import {BrewService} from '@/services/brew-service'
-import {useLocalSearchParams} from 'expo-router'
+import Ionicons from "@expo/vector-icons/Ionicons";
+import {Link, useLocalSearchParams} from 'expo-router'
 import React, {useEffect, useState} from 'react'
-import {ActivityIndicator, Text, View} from 'react-native'
+import {ActivityIndicator, Text, TouchableOpacity, View} from 'react-native'
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
-export default function BrewDetail() {
+const BrewDetail: React.FC = () => {
     const {id} = useLocalSearchParams();
 
     const [brew, setBrew] = useState<Brew | null>(null)
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        const load = async () => {
-            if (!id) return
-            const result = await BrewService.getBrewById(Number(id))
-            setBrew(result)
-            setLoading(false)
+        if (!id) {
+            return
         }
-        load()
+
+        BrewService
+            .getBrewById(Number(id))
+            .then(brew => {
+                setBrew(brew)
+                setLoading(false)
+            })
+
     }, [id])
 
     if (loading) {
@@ -38,16 +44,47 @@ export default function BrewDetail() {
 
     return (
         <>
-            <Text className="text-3xl font-semibold text-center">{brew.name}</Text>
-            <Text>State: {brew.state}</Text>
-            <Text>Created At: {brew.createdAt.toDateString()}</Text>
-            {brew.firstFermentationEnd && (
-                <Text>F1 Ends: {brew.firstFermentationEnd.toDateString()}</Text>
-            )}
-            {brew.secondFermentationEnd && (
-                <Text>F2 Ends: {brew.secondFermentationEnd.toDateString()}</Text>
-            )}
-            {brew.notes && <Text>{brew.notes}</Text>}
+            <BrewDetailHeader/>
+
+            <View className="flex-1 px-4">
+                <Text className="text-5xl font-bold text-brown-900">{brew.name}</Text>
+
+                <Text>State: {brew.state}</Text>
+                <Text>Created At: {brew.createdAt.toDateString()}</Text>
+                {brew.firstFermentationEnd && (
+                    <Text>F1 Ends: {brew.firstFermentationEnd.toDateString()}</Text>
+                )}
+                {brew.secondFermentationEnd && (
+                    <Text>F2 Ends: {brew.secondFermentationEnd.toDateString()}</Text>
+                )}
+                {brew.notes && <Text>{brew.notes}</Text>}
+            </View>
         </>
+    )
+}
+
+export default BrewDetail
+
+const BrewDetailHeader: React.FC = () => {
+    const insets = useSafeAreaInsets();
+
+    return (
+        <View className="px-4 "
+              style={{
+                  paddingTop: insets.top,
+              }}
+        >
+            <View className="flex-row items-center p-0 mt-4 mb-10">
+                <TouchableOpacity activeOpacity={0.8}>
+                    <Link href="/">
+                        <View className="w-12 h-12 rounded-full border border-brown-900 justify-center items-center">
+                            <Ionicons name="chevron-back" size={20} color="brown-500"/>
+                        </View>
+                    </Link>
+                </TouchableOpacity>
+                <Text className="text-brown-900 text-xl font-semibold ml-4">Brew details</Text>
+            </View>
+
+        </View>
     )
 }
