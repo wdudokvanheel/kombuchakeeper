3d045db3e308430e6e1e4f7a7b21682f125cc93d
wesley <wesley@bitechular.com>
2025-06-12 21:18:29 +0200

    Implement extend fermentation page

---
+++ app/batch/[id]/actions/components/action-body.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {View} from "react-native";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+
+type ActionBodyProps = {
+    children: React.ReactNode
+}
+
+const ActionBody = ({children}: ActionBodyProps) => {
+    const insets = useSafeAreaInsets()
+
+    return (
+        <View
+            className="flex-1 p-4 pt-8 items-center"
+            style={{
+                paddingBottom: insets.bottom + 16
+            }}
+        >
+            {children}
+        </View>
+    )
+}
+
+export default ActionBody
--- app/batch/[id]/actions/extend.tsx
+++ app/batch/[id]/actions/extend.tsx
@@ -1,26 +1,70 @@
-import Text from "@/ui/components/text";
-import {NativeWindColors} from "@/ui/nativewind";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import {useRouter} from "expo-router";
-import React from "react";
-import {TouchableOpacity, View} from "react-native";
+import ActionBody from "@/app/batch/[id]/actions/components/action-body";
+import {useBatch} from "@/app/batch/[id]/batch-context"
+import {BatchState} from "@/models/batch"
+import {BatchService} from "@/services/batch-service"
+import SimpleHeader from "@/ui/components/simple-header"
+import Text from "@/ui/components/text"
+import NumberSelector from "@/ui/components/wheel-picker"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import {useRouter} from "expo-router"
+import React from "react"
+import {TouchableOpacity, View} from "react-native"
 
 const ExtendFermentation: React.FC = () => {
     const router = useRouter()
-    const handleBack = () => router.back()
+    const batch = useBatch()
+
+    const [duration, setDuration] = React.useState(10)
+
+    const handleExtend = () => {
+        console.log(`Extending batch ${batch.id}[${batch.state}] by ${duration} days`)
+
+        switch (batch.state) {
+            case BatchState.F1: {
+                const endDate = batch.firstFermentationEnd || new Date()
+                endDate.setDate(endDate.getDate() + duration)
+                batch.firstFermentationEnd = endDate
+                BatchService.updateBatch(batch)
+                break
+            }
+            case "F2": {
+                const endDate = batch.secondFermentationEnd || new Date()
+                endDate.setDate(endDate.getDate() + duration)
+                batch.firstFermentationEnd = endDate
+                BatchService.updateBatch(batch)
+                break
+            }
+            default:
+                break
+        }
+
+        // TODO Handle this is a less hacky way?
+        router.back()
+        router.back()
+    }
 
     return (
-        <View className="flex-col gap-4 p-4">
-            <TouchableOpacity activeOpacity={0.8} onPress={handleBack}>
-                <View className="w-12 h-12 rounded-full border border-b-brown-800 justify-center items-center">
-                    <Ionicons name="chevron-back" size={20} color={NativeWindColors.brown[800]}/>
-                </View>
-            </TouchableOpacity>
+        <>
+            <SimpleHeader title="Extend fermentation"/>
 
-            <Text className="text-brown-800 mt-8 mb-2 text-4xl font-extrabold">
-                Extend fermentation
-            </Text>
-        </View>
+            <ActionBody>
+                <Text className="text-4xl text-brown-800 font-extrabold mb-8 text-center">
+                    How many extra days to ferment?
+                </Text>
+
+                <NumberSelector onChange={setDuration} value={duration} itemHeight={150} width={220}/>
+
+                <View>
+                    <TouchableOpacity
+                        onPress={handleExtend}
+                        className="bg-brown-800 rounded-[64px] py-4 px-6 flex-row items-center justify-center"
+                    >
+                        <Text className="text-white text-2xl font-semibold">Extend fermentation</Text>
+                        <Ionicons name="add" size={28} color="white" className="ml-2"/>
+                    </TouchableOpacity>
+                </View>
+            </ActionBody>
+        </>
     )
 }
 export default ExtendFermentation
