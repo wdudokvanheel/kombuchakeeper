4d02828b0b6fe1ddd6c3cc43c84b475bd51b6ec6
wesley <wesley@bitechular.com>
2025-06-13 11:20:55 +0200

    Implement batch complete screen

--- app/batch/[id]/actions/complete.tsx
+++ app/batch/[id]/actions/complete.tsx
@@ -1,5 +1,9 @@
+import ActionBody from "@/app/batch/[id]/actions/components/action-body"
+import {useBatch} from "@/contexts/batch-context"
+import {BatchState} from "@/models/batch"
+import {BatchService} from "@/services/batch-service"
+import SimpleHeader from "@/ui/components/simple-header"
 import Text from "@/ui/components/text"
-import {NativeWindColors} from "@/ui/nativewind"
 import Ionicons from "@expo/vector-icons/Ionicons"
 import {useRouter} from "expo-router"
 import React from "react"
@@ -7,20 +11,40 @@ import {TouchableOpacity, View} from "react-native"
 
 const CompleteBatch = () => {
     const router = useRouter()
-    const handleBack = () => router.back()
+    const batch = useBatch()
+
+    const handleComplete = () => {
+        console.log(`Completing batch #${batch.id}[${batch.state}]`)
+
+        batch.state = BatchState.Bottled
+        BatchService.updateBatch(batch)
+
+        // TODO Handle this is a less hacky way?
+        router.back()
+        router.back()
+    }
 
     return (
-        <View className="flex-col gap-4 p-4">
-            <TouchableOpacity activeOpacity={0.8} onPress={handleBack}>
-                <View className="w-12 h-12 rounded-full border border-b-brown-800 justify-center items-center">
-                    <Ionicons name="chevron-back" size={20} color={NativeWindColors.brown[800]}/>
-                </View>
-            </TouchableOpacity>
+        <>
+            <SimpleHeader title="Batch complete" />
 
-            <Text className="text-brown-800 mt-8 mb-2 text-4xl font-extrabold">
-                Complete batch
-            </Text>
-        </View>
+            <ActionBody>
+                <View className="flex-1 justify-between">
+                    <Text className="text-4xl text-brown-800 font-extrabold mb-8 text-center">
+                    </Text>
+
+                    <View>
+                        <TouchableOpacity
+                            onPress={handleComplete}
+                            className="bg-brown-800 rounded-[64px] py-4 px-6 flex-row items-center justify-center"
+                        >
+                            <Text className="text-white text-2xl font-semibold">Mark batch as complete</Text>
+                            <Ionicons name="checkmark" size={28} color="white" className="ml-2"/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ActionBody>
+        </>
     )
 }
 export default CompleteBatch
