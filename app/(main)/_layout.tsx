65d05b20a761181b3506ebea9651bede102f23e4
wesley <wesley@bitechular.com>
2025-06-15 16:37:22 +0200

    Fix app background color

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -11,23 +11,25 @@ const MainLayout = () => {
 
     return (
         <>
-            <ThemedHeader background={NativeWindColors.brown[800]} useSafeArea={true}>
-                <Text className="text-brown-100 text-3xl text-center p-0 mb-4 uppercase">
-                    Kombucha Keeper
-                </Text>
-            </ThemedHeader>
+            <View className="flex-1 bg-brown-100">
+                <ThemedHeader background={NativeWindColors.brown[800]} useSafeArea={true}>
+                    <Text className="text-brown-100 text-3xl text-center p-0 mb-4 uppercase">
+                        Kombucha Keeper
+                    </Text>
+                </ThemedHeader>
 
-            <View className="flex-1">
-                <Slot/>
-            </View>
+                <View className="flex-1">
+                    <Slot/>
+                </View>
 
-            <View className="mx-0">
-                <MenuBar
-                    height={115}
-                    dipWidth={160}
-                    cornerRadius={100}
-                    onPress={() => router.push("/batch/new")}
-                />
+                <View className="mx-0">
+                    <MenuBar
+                        height={115}
+                        dipWidth={160}
+                        cornerRadius={100}
+                        onPress={() => router.push("/batch/new")}
+                    />
+                </View>
             </View>
         </>
     )
