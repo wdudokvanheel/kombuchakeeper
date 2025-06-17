d1fe9032dd6ad41bde360b7227052296dbb46f7f
wesley <wesley@bitechular.com>
2025-06-17 12:08:57 +0200

    Improve batch list UI

--- app/(main)/batch-list.tsx
+++ app/(main)/batch-list.tsx
@@ -1,12 +1,12 @@
 import {Batch, BatchState} from "@/models/batch"
 import {BatchStateColor, BatchStateLabelColor} from "@/ui/batch-state-color"
+import AdaptiveProgressCircle from "@/ui/components/adaptive-progress-circle"
 import Text from "@/ui/components/text"
 import {NativeWindColors} from "@/ui/nativewind"
 import Ionicons from "@expo/vector-icons/Ionicons"
 import {Link} from "expo-router"
 import React from 'react'
 import {FlatList, TouchableOpacity, View} from "react-native"
-import {CircularProgressBase} from "react-native-circular-progress-indicator"
 
 const BatchStateDarkColor: Record<BatchState, string> = {
     [BatchState.F1]: NativeWindColors.yellow[600],
@@ -57,9 +57,9 @@ const BatchListItem = ({batch}: BatchListItemProps) => {
     return (
         <Link href={`/batch/${batch.id}`} asChild>
             <TouchableOpacity activeOpacity={0.5}>
-                <View className="shadow-[0_6px_8px_rgba(0,0,0,0.05)] rounded-[38px] p-4 mt-4 bg-white">
-                    <View className=" flex-row">
-                        <View className=" w-1/4 p-2">
+                <View className="shadow-[0_6px_8px_rgba(0,0,0,0.05)] rounded-[38px] p-5 mt-4 bg-white">
+                    <View className="flex-row">
+                        <View className="w-1/4 h-full">
                             <View
                                 className={`items-center justify-center rounded-3xl flex-1`}
                                 style={{
@@ -68,7 +68,7 @@ const BatchListItem = ({batch}: BatchListItemProps) => {
                             >
                                 {(batch.state === BatchState.F1 || batch.state === BatchState.F2) && (
                                     <Text
-                                        className="text-3xl font-bold"
+                                        className="text-4xl font-bold tracking-widest"
                                         style={{color: BatchStateLabelColor[batch.state]}}
                                     >
                                         {batch.state}
@@ -83,51 +83,56 @@ const BatchListItem = ({batch}: BatchListItemProps) => {
                             </View>
                         </View>
 
-                        <View className="w-2/4 items-start ps-2 pt-2 pb-2 justify-between gap-0">
-                            <Text className="text-xl font-medium" numberOfLines={1}
-                                  style={{color: batch.isCurrentFermentationComplete() && !batch.hasEnded() ? NativeWindColors.green[800] : NativeWindColors.brown[800]}}>
-                                {batch.name}
-                            </Text>
-
-                            {batch.getDaysLeft() == 0 && !batch.hasEnded() &&
-                                <Text className="text-lg font-bold text-white rounded-full bg-green-600 py-0 px-2"
-                                      numberOfLines={1}>
-                                    {batch.state == BatchState.F1 ? 'Ready for F2' : 'Ready to be bottled'}
+                        <View className="flex-1 flex-row">
+                            <View className="flex-1 items-start px-4 justify-between gap-0">
+                                <Text className="text-xl font-medium" numberOfLines={1}
+                                      style={{color: batch.isCurrentFermentationComplete() && !batch.hasEnded() ? NativeWindColors.green[800] : NativeWindColors.brown[800]}}>
+                                    {batch.name}
                                 </Text>
-                            }
-                            {(batch.getDaysLeft() != 0 || batch.hasEnded()) &&
-                                <Text className="text-lg font-medium text-brown-800/65" numberOfLines={1}>
-                                    Started on {formattedDate}
-                                </Text>
-                            }
-                        </View>
 
-                        <View className="w-1/4 items-end py-2 px-2">
-                            <CircularProgressBase
-                                key={`${batch.id}-${batch.getDaysSinceStart()}-${batch.getCurrentFermentationDuration()}`}
-                                radius={32}
-                                activeStrokeWidth={16}
-                                inActiveStrokeWidth={16}
-                                activeStrokeColor={batch.isCurrentFermentationComplete() ? NativeWindColors.green[600] : BatchStateColor[batch.state]}
-                                inActiveStrokeColor={NativeWindColors.gray[200]}
-                                value={batch.getDaysSinceStart() || 0}
-                                maxValue={batch.getCurrentFermentationDuration()}
-                            >
-                                {(batch.state === BatchState.F1 || batch.state === BatchState.F2) && (
-                                    !batch.isCurrentFermentationComplete() ?
-                                        <Text
-                                            className="text-xl font-extrabold m-0 p-0 "
-                                            style={{
-                                                color: BatchStateDarkColor[batch.state]
-                                            }}
-                                        >
-                                            {(batch.getDaysLeft() || 0) > 0 ? batch.getDaysLeft() : ''}
-                                        </Text>
-                                        :
-                                        <Ionicons name="checkmark-outline" size={28}
+                                {batch.getDaysLeft() == 0 && !batch.hasEnded() &&
+                                    <Text className="text-lg font-bold text-white rounded-full bg-green-600 py-0 px-2"
+                                          numberOfLines={1}>
+                                        {batch.state == BatchState.F1 ? 'Ready for F2' : 'Ready to be bottled'}
+                                    </Text>
+                                }
+                                {(batch.getDaysLeft() != 0 || batch.hasEnded()) &&
+                                    <Text className="text-lg font-medium text-brown-800/65" numberOfLines={1}>
+                                        Started on {formattedDate}
+                                    </Text>
+                                }
+                            </View>
+
+                            <View className="p-0 w-1/4">
+                                <AdaptiveProgressCircle
+                                    className="aspect-square"
+                                    activeStrokeWidth={16}
+                                    inActiveStrokeWidth={16}
+                                    activeStrokeColor={batch.isCurrentFermentationComplete() ? NativeWindColors.green[600] : BatchStateColor[batch.state]}
+                                    inActiveStrokeColor={NativeWindColors.gray[200]}
+                                    value={batch.getDaysSinceStart() || 0}
+                                    maxValue={batch.getCurrentFermentationDuration()}
+                                >
+                                    {(batch.state === BatchState.F1 || batch.state === BatchState.F2) && (
+                                        !batch.isCurrentFermentationComplete() ?
+                                            <Text
+                                                className="text-2xl font-extrabold m-0 p-0 "
+                                                style={{
+                                                    color: BatchStateDarkColor[batch.state]
+                                                }}
+                                            >
+                                                {(batch.getDaysLeft() || 0) > 0 ? batch.getDaysLeft() : ''}
+                                            </Text>
+                                            :
+                                            <Ionicons name="checkmark-outline" size={36}
+                                                      color={NativeWindColors.green[800]}/>
+                                    )}
+                                    {batch.hasEnded() &&
+                                        <Ionicons name="checkmark-outline" size={36}
                                                   color={NativeWindColors.green[800]}/>
-                                )}
-                            </CircularProgressBase>
+                                    }
+                                </AdaptiveProgressCircle>
+                            </View>
                         </View>
                     </View>
                 </View>
