6cf1e6483e529dba8a372a7b1fc7c8a1634d2a2f
wesley <wesley@bitechular.com>
2025-06-19 16:10:45 +0200

    Add logo SVG to about page

--- app/settings/about.tsx
+++ app/settings/about.tsx
@@ -1,7 +1,7 @@
 import {expo} from "@/app.json"
 import SimpleHeader from "@/ui/components/simple-header"
 import Text from "@/ui/components/text"
-import {Image} from "expo-image"
+import JarGraphic from "@/ui/graphics/logo-graphic"
 import React from "react"
 import {Linking, Platform, View} from "react-native"
 import {useSafeAreaInsets} from "react-native-safe-area-context"
@@ -10,36 +10,27 @@ const About = () => {
     const insets = useSafeAreaInsets()
 
     return (
-        <><View
+        <View
             className="flex-1"
-            style={[{
-                paddingBottom: Math.max(insets.bottom, 16),
-            }]}
+            style={{paddingBottom: Math.max(insets.bottom, 16)}}
         >
             <SimpleHeader title="About"/>
-            <View className="p-4 flex-col flex-1 justify-between">
-                <View className="h-1/2">
-                    <Image
-                        contentPosition={"center"}
-                        contentFit={"contain"}
-                        style={{
-                            width: '100%',
-                            height: '100%',
-                        }}
-                        className="bg-white"
-                        source={require('@/assets/images/fail.png')}
-                    />
+
+            <View className="flex-1 px-4 pb-4">
+                <View className="flex-1  items-center justify-center">
+                    <JarGraphic/>
                 </View>
 
-                <InfoPanel/>
+                <InfoPanel className="mt-6"/>
             </View>
         </View>
-        </>
     )
 }
 export default About
 
-const InfoPanel = () => {
+type InfoProps = { className?: string }
+
+const InfoPanel = ({className}: InfoProps) => {
     const appVersion = expo.version ?? "1.0.0"
     const platformPrefix = Platform.OS === "ios" ? "iOS" : "Android"
 
@@ -48,13 +39,12 @@ const InfoPanel = () => {
     const URL_SOURCE = `https://github.com/wdudokvanheel/kombuchakeeper/tree/${platformPrefix.toLowerCase()}-v${appVersion}`
 
     return (
-        <View className="p-4 bg-white rounded-[2rem]">
+        <View className={`p-4 bg-white rounded-[2rem] ${className ?? ""}`}>
             <Text className="text-3xl font-semibold text-gray-900">
                 Kombucha Keeper
             </Text>
             <Text className="text-lg text-gray-800">
-                {platformPrefix} client
-                v{appVersion}
+                {platformPrefix} client · v{appVersion}
             </Text>
 
             <Text
